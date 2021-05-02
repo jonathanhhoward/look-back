abe17cca756ba755ce5f7f85f0266a476353ae11
Jonathan Howard <2773262@gmail.com>
2021-05-02 13:52:25 -0400

    Add styling to TitleBar

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,5 +1,11 @@
-import { AppBar } from "@material-ui/core";
+import { AppBar, Container } from "@material-ui/core";
 
 export function TitleBar() {
-  return <AppBar position="sticky">Look Back</AppBar>;
+  return (
+    <AppBar position="sticky">
+      <Container>
+        <h1>Look Back</h1>
+      </Container>
+    </AppBar>
+  );
 }
