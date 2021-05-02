38d6fdbe9fd70fcc007a5eb2c132cfd002edcbae
Jonathan Howard <2773262@gmail.com>
2021-05-02 14:41:28 -0400

    Add subtitle for 14-Day inspections

--- src/components/Inspections/Inspections.test.tsx
+++ src/components/Inspections/Inspections.test.tsx
@@ -6,3 +6,9 @@ test("renders the component title", () => {
   const inspections = screen.getByText(/^inspections$/i);
   expect(inspections).toHaveTextContent(/^inspections$/i);
 });
+
+test("renders the subtitle for 14-day inspections", () => {
+  render(<Inspections />);
+  const days14 = screen.getByText(/^14-day$/i);
+  expect(days14).toHaveTextContent(/^14-day$/i);
+});
--- src/components/Inspections/Inspections.tsx
+++ src/components/Inspections/Inspections.tsx
@@ -5,6 +5,7 @@ export function Inspections() {
     <Paper>
       <Container>
         <h2>Inspections</h2>
+        <h3>14-Day</h3>
       </Container>
     </Paper>
   );
