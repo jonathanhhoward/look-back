ca70420c10b2f8dbc8dea2d6fd092d93616e609a
Jonathan Howard <2773262@gmail.com>
2021-05-02 14:44:01 -0400

    Add subtitle for 2-Day inspections

--- src/components/Inspections/Inspections.test.tsx
+++ src/components/Inspections/Inspections.test.tsx
@@ -12,3 +12,9 @@ test("renders the subtitle for 14-day inspections", () => {
   const days14 = screen.getByText(/^14-day$/i);
   expect(days14).toHaveTextContent(/^14-day$/i);
 });
+
+test("renders the subtitle for 2-day inspections", () => {
+  render(<Inspections />);
+  const days2 = screen.getByText(/^2-day$/i);
+  expect(days2).toHaveTextContent(/^2-day$/i);
+});
--- src/components/Inspections/Inspections.tsx
+++ src/components/Inspections/Inspections.tsx
@@ -6,6 +6,7 @@ export function Inspections() {
       <Container>
         <h2>Inspections</h2>
         <h3>14-Day</h3>
+        <h3>2-Day</h3>
       </Container>
     </Paper>
   );
