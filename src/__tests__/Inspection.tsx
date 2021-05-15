9ff008341aacd2d68bfd264474b4946c471b2124
Jonathan Howard <2773262@gmail.com>
2021-05-15 08:04:14 -0400

    Rename title

--- src/__tests__/Inspection.tsx
+++ src/__tests__/Inspection.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Inspection } from "components";
 
 test("renders the inspection title", () => {
-  render(<Inspection title="A Title" />);
-  const title = screen.getByText("A Title");
-  expect(title).toHaveTextContent("A Title");
+  render(<Inspection title="Title" />);
+  const title = screen.getByText("Title");
+  expect(title).toHaveTextContent("Title");
 });
