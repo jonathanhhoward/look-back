643cfbe0bd7d63eba6af32bffb17c6bbc4e677e4
Jonathan Howard <2773262@gmail.com>
2021-05-15 08:01:56 -0400

    Remove tests for subtitles

--- src/__tests__/InspectionList.tsx
+++ src/__tests__/InspectionList.tsx
@@ -6,15 +6,3 @@ test("renders the component title", () => {
   const inspections = screen.getByText(/^inspection$/i);
   expect(inspections).toHaveTextContent(/^inspection$/i);
 });
-
-test("renders the subtitle for 14-day inspections", () => {
-  render(<InspectionList />);
-  const days14 = screen.getByText(/^14-day$/i);
-  expect(days14).toHaveTextContent(/^14-day$/i);
-});
-
-test("renders the subtitle for 2-day inspections", () => {
-  render(<InspectionList />);
-  const days2 = screen.getByText(/^2-day$/i);
-  expect(days2).toHaveTextContent(/^2-day$/i);
-});
