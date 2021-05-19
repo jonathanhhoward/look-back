84b516255999ed9031bae86f16a4b2ff061bb273
Jonathan Howard <2773262@gmail.com>
2021-05-19 13:10:24 -0400

    Adds new deferral on add button click with incremented title

--- src/__tests__/DeferralList.tsx
+++ src/__tests__/DeferralList.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { DeferralList } from "components";
+import userEvent from "@testing-library/user-event";
 
 test("renders the component title", () => {
   render(<DeferralList />);
@@ -11,4 +12,18 @@ test("renders an add button", () => {
   render(<DeferralList />);
   const button = screen.getByRole("button");
   expect(button).toBeInTheDocument();
+});
+
+test("renders new deferral on add button click with incremented title", () => {
+  render(<DeferralList />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  userEvent.click(button);
+  userEvent.click(button);
+  const deferral1 = screen.getByText(/^deferral1$/i);
+  const deferral2 = screen.getByText(/^deferral2$/i);
+  const deferral3 = screen.getByText(/^deferral3$/i);
+  expect(deferral1).toBeInTheDocument();
+  expect(deferral2).toBeInTheDocument();
+  expect(deferral3).toBeInTheDocument();
 });
--- src/components/DeferralList.tsx
+++ src/components/DeferralList.tsx
@@ -1,19 +1,41 @@
+import { useState } from "react";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export function DeferralList() {
+  const [deferrals, setDeferrals] = useState<string[]>([]);
+
+  function handleClick() {
+    const nextIndex = deferrals.length + 1;
+    setDeferrals(deferrals.concat(`Deferral${nextIndex}`));
+  }
+
   return (
     <Card elevation={0} square>
       <CardHeader
         title="Deferral"
         action={
-          <IconButton>
+          <IconButton onClick={handleClick}>
             <AddIcon />
           </IconButton>
         }
       />
+      <CardContent>
+        {deferrals.map((deferral, i) => (
+          <Accordion key={i}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="subtitle1">{deferral}</Typography>
+            </AccordionSummary>
+          </Accordion>
+        ))}
+      </CardContent>
     </Card>
   );
 }
