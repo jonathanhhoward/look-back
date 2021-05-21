0830998fdd1ba87a243e068467807bb07a9e5974
Jonathan Howard <2773262@gmail.com>
2021-05-20 21:56:33 -0400

    Move render() to beforeEach()

--- src/__tests__/Deferral.tsx
+++ src/__tests__/Deferral.tsx
@@ -1,26 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import { Deferral } from "components";
 
-test("renders the title", () => {
+beforeEach(() => {
   render(<Deferral />);
+});
+
+test("renders the title", () => {
   const heading = screen.getByText(/^new deferral$/i);
   expect(heading).toBeInTheDocument();
 });
 
 test("renders input to select type", () => {
-  render(<Deferral />);
   const select = screen.getByLabelText(/^type$/i);
   expect(select).toBeInTheDocument();
 });
 
 test("renders input to enter deferral number", () => {
-  render(<Deferral />);
   const textfield = screen.getByLabelText(/^number$/i);
   expect(textfield).toBeInTheDocument();
 });
 
 test("renders input to select deferral category", () => {
-  render(<Deferral />);
   const select = screen.getByLabelText(/^category$/i);
   expect(select).toBeInTheDocument();
 });
