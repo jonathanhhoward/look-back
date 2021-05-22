7c304245fe9355a33da9c0e47936be5cd89fdedb
Jonathan Howard <2773262@gmail.com>
2021-05-22 11:40:55 -0400

    Add grid

--- src/components/Deferral.tsx
+++ src/components/Deferral.tsx
@@ -2,6 +2,7 @@ import Accordion from "@material-ui/core/Accordion";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import FormControl from "@material-ui/core/FormControl";
+import Grid from "@material-ui/core/Grid";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
@@ -15,27 +16,39 @@ export function Deferral() {
         <Typography variant="subtitle1">New Deferral</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <FormControl>
-          <InputLabel id="type-label">Type</InputLabel>
-          <Select variant="filled" labelId="type-label" value="" />
-        </FormControl>
-        <TextField variant="filled" id="number" label="Number" />
-        <FormControl>
-          <InputLabel id="category-label">Category</InputLabel>
-          <Select variant="filled" labelId="category-label" value="" />
-        </FormControl>
-        <TextField
-          type="number"
-          variant="filled"
-          id="duration"
-          label="Duration"
-        />
-        <TextField
-          type="date"
-          variant="filled"
-          id="date"
-          label="Deferral Date"
-        />
+        <Grid container spacing={2} justify="center">
+          <Grid item>
+            <FormControl>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select variant="filled" labelId="type-label" value="" />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <TextField variant="filled" id="number" label="Number" />
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <InputLabel id="category-label">Category</InputLabel>
+              <Select variant="filled" labelId="category-label" value="" />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <TextField
+              type="number"
+              variant="filled"
+              id="duration"
+              label="Duration"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              type="date"
+              variant="filled"
+              id="date"
+              label="Deferral Date"
+            />
+          </Grid>
+        </Grid>
       </AccordionDetails>
     </Accordion>
   );
