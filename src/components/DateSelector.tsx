3284e1ac43c5c4771f7290fad64356ae507fde57
Jonathan Howard <2773262@gmail.com>
2021-05-24 16:57:00 -0400

    Add styling to inputs

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,6 +1,7 @@
 import { SetStateAction } from "react";
 import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
+import { makeStyles } from "@material-ui/core/styles";
 import LuxonUtils from "@date-io/luxon";
 
 interface DateSelectorProps {
@@ -10,17 +11,32 @@ interface DateSelectorProps {
   setDate: (
     value: SetStateAction<MaterialUiPickersDate> | MaterialUiPickersDate
   ) => void;
+  width?: string;
 }
+
+interface DateSelectorStyleProps {
+  width?: string;
+}
+
+const useStyles = makeStyles({
+  datepicker: ({ width }: DateSelectorStyleProps) => ({
+    width: width,
+  }),
+});
 
 export function DateSelector({
   date,
   label,
   pickerId,
   setDate,
+  width,
 }: DateSelectorProps) {
+  const classes = useStyles({ width });
+
   return (
     <MuiPickersUtilsProvider utils={LuxonUtils}>
       <DatePicker
+        className={classes.datepicker}
         disableFuture
         disableToolbar
         format="MM/dd/yyyy"
--- src/components/Deferral.tsx
+++ src/components/Deferral.tsx
@@ -11,47 +11,85 @@ import Select from "@material-ui/core/Select";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import { DateSelector } from "components";
 
+const useStyles = makeStyles(({ palette }: Theme) =>
+  createStyles({
+    title: {
+      flexBasis: "50%",
+    },
+    subtitle: {
+      color: palette.text.secondary,
+    },
+    label: {
+      paddingLeft: "8px",
+    },
+    input: {
+      width: "100%",
+    },
+    splitInput: {
+      width: "50%",
+    },
+  })
+);
+
 export function Deferral() {
+  const classes = useStyles();
+
   return (
     <Accordion>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="subtitle1">New Deferral</Typography>
-        <Typography variant="subtitle2">Item Number</Typography>
+        <Typography className={classes.title} variant="subtitle1">
+          New Deferral
+        </Typography>
+        <Typography className={classes.subtitle} variant="subtitle1">
+          Item Number
+        </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Grid container spacing={2} justify="center">
-          <Grid item>
-            <FormControl>
-              <InputLabel id="type-label">Type</InputLabel>
-              <Select variant="filled" labelId="type-label" value="" />
+        <Grid container spacing={2}>
+          <Grid item xs={3} md={1}>
+            <FormControl className={classes.input}>
+              <InputLabel className={classes.label} id="type">
+                Type
+              </InputLabel>
+              <Select labelId="type" value="" variant="filled" />
             </FormControl>
           </Grid>
-          <Grid item>
-            <FormControl>
-              <InputLabel htmlFor="number">Number</InputLabel>
-              <FilledInput type="text" id="number" />
+          <Grid item xs={9} md={5}>
+            <FormControl className={classes.input}>
+              <InputLabel className={classes.label} htmlFor="number">
+                Number
+              </InputLabel>
+              <FilledInput id="number" type="text" />
             </FormControl>
           </Grid>
-          <Grid item>
-            <FormControl>
-              <InputLabel id="category-label">Category</InputLabel>
-              <Select variant="filled" labelId="category-label" value="" />
+          <Grid item xs={5} md={2}>
+            <FormControl className={classes.splitInput}>
+              <InputLabel className={classes.label} id="category">
+                Category
+              </InputLabel>
+              <Select labelId="category" value="" variant="filled" />
+            </FormControl>
+            <FormControl className={classes.splitInput}>
+              <InputLabel className={classes.label} htmlFor="duration">
+                Duration
+              </InputLabel>
+              <FilledInput
+                id="duration"
+                inputProps={{ min: "1" }}
+                type="number"
+              />
             </FormControl>
           </Grid>
-          <Grid item>
-            <FormControl>
-              <InputLabel htmlFor="duration">Duration</InputLabel>
-              <FilledInput type="number" id="duration" />
-            </FormControl>
-          </Grid>
-          <Grid item>
+          <Grid item xs={7} md={4}>
             <DateSelector
               date={null}
               label="Deferral Date"
               pickerId="deferral-date"
               setDate={() => {}}
+              width="100%"
             />
           </Grid>
         </Grid>
