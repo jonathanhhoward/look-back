67578e4fa09f9829d04f975391908d193f8c2346
Jonathan Howard <2773262@gmail.com>
2021-05-25 22:32:08 -0400

    Move render() to beforeEach()

--- src/__tests__/DeferralList.tsx
+++ src/__tests__/DeferralList.tsx
@@ -2,20 +2,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { DeferralList } from "components";
 
-test("renders the component title", () => {
+beforeEach(() => {
   render(<DeferralList />);
+});
+
+test("renders the component title", () => {
   const title = screen.getByText(/^deferral$/i);
   expect(title).toHaveTextContent(/^deferral$/i);
 });
 
 test("renders an add button", () => {
-  render(<DeferralList />);
   const button = screen.getByLabelText(/^add deferral$/i);
   expect(button).toBeInTheDocument();
 });
 
 test("renders new deferral on add button click", () => {
-  render(<DeferralList />);
   const button = screen.getByLabelText(/^add deferral$/i);
   userEvent.click(button);
   const deferral = screen.getByText(/^new deferral$/i);
@@ -23,7 +24,6 @@ test("renders new deferral on add button click", () => {
 });
 
 test("removes a deferral when its delete button is clicked", () => {
-  render(<DeferralList />);
   const addButton = screen.getByLabelText(/^add deferral$/i);
   userEvent.click(addButton);
   const deleteButton = screen.getByLabelText(/^delete deferral$/i);
