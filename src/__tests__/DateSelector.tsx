a0d3be6438632cfb03185d139cf0b1be2612a658
Jonathan Howard <2773262@gmail.com>
2021-05-25 23:15:24 -0400

    Add test for datepicker

---
+++ src/__tests__/DateSelector.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { DateSelector } from "components";
+
+test("renders a datepicker", () => {
+  render(
+    <DateSelector
+      handleChange={jest.fn()}
+      label="picker label"
+      pickerId="picker"
+      value=""
+    />
+  );
+  const datepicker = screen.getByLabelText(/^picker label$/i);
+  expect(datepicker).toBeInTheDocument();
+});
