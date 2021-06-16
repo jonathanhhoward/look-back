3a4fe94c4f9218307809f8ed7600c412b8b7e293
Jonathan Howard <2773262@gmail.com>
2021-06-15 20:41:18 -0400

    Add dark mode

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,28 @@
+import { useMemo } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { DeferralList, InspectionList, TitleBar } from "components";
 import "@fontsource/roboto";
 
 export function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <TitleBar />
       <InspectionList />
       <DeferralList />
-    </>
+    </ThemeProvider>
   );
 }
